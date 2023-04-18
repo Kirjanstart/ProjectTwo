2c3f75e0d67569fd304e4cabcffc5efbd8edaea4
k.lavkin <kirjanstart@bk.ru>
2023-04-18 14:48:27 +0300

    Done with envelop and paper

--- Lesson 3/02_envelop.py
+++ Lesson 3/02_envelop.py
@@ -19,6 +19,10 @@ paper_x, paper_y = 8, 9
 # paper_x, paper_y = 11, 9
 # paper_x, paper_y = 9, 11
 # (просто раскоментировать нужную строку и проверить свой код)
+if (envelop_x >= paper_x and envelop_y >= paper_y) or (envelop_x >= paper_y and envelop_y >= paper_x):
+    print('Да')
+else:
+    print('Нет')
 
 # TODO здесь ваш код
 
