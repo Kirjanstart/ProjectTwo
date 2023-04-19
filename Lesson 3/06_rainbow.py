afba00e6172ec091ab2abbd315090c89aa5f62e3
k.lavkin <kirjanstart@bk.ru>
2023-04-19 15:46:32 +0300

    Straight rainbow is done

--- Lesson 3/06_rainbow.py
+++ Lesson 3/06_rainbow.py
@@ -9,6 +9,23 @@ rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN
 
 # Нарисовать радугу: 7 линий разного цвета толщиной 4 с шагом 5 из точки (50, 50) в точку (350, 450)
 # TODO здесь ваш код
+sd.resolution = (1200, 600)
+x = 50
+y = 350
+for color in rainbow_colors:
+    st_point = sd.get_point(x, 50)
+    fn_point = sd.get_point(y, 450)
+    x+=5
+    y+=5
+    sd.line(st_point, fn_point, color=color,  width=4)
+
+
+
+
+
+
+
+
 
 # Усложненное задание, делать по желанию.
 # Нарисовать радугу дугами от окружности (cсм sd.circle) за нижним краем экрана,
