141c52f938b1baf2ce1d2ce519093da1dbf389f5
k.lavkin <kirjanstart@bk.ru>
2023-04-19 17:57:29 +0300

    Draw one smile.

--- Lesson 3/08_smile.py
+++ Lesson 3/08_smile.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # (определение функций)
-import simple_draw
+import simple_draw as sd
 
 # Написать функцию отрисовки смайлика в произвольной точке экрана
 # Форма рожицы-смайлика на ваше усмотрение
@@ -9,5 +9,25 @@ import simple_draw
 # Вывести 10 смайликов в произвольных точках экрана.
 
 # TODO здесь ваш код
+def smile(coordinate_x, coordinate_y, color):
+    point = sd.get_point(coordinate_x, coordinate_y)
+    left_coordinate_x = coordinate_x - 20
+    left_coordinate_y = coordinate_y + 15
+    right_coordinate_x = coordinate_x + 20
+    right_coordinate_y = coordinate_y + 15
+    right_point = sd.get_point(right_coordinate_x, right_coordinate_y)
+    left_point = sd.get_point(left_coordinate_x, left_coordinate_y)
+    mouth_coordinate_x = coordinate_x
+    mouth_coordinate_y = coordinate_y - 20
+    mouth_point = sd.get_point(mouth_coordinate_x, mouth_coordinate_y)
+    sd.circle(center_position=mouth_point, radius=15, color=color, width=3)
+    sd.circle(center_position=right_point, radius=5,color=color, width=3)
+    sd.circle(center_position=left_point, radius=5,color=color, width=3)
+    sd.circle(center_position=point, radius=50,color=color, width=3)
 
-simple_draw.pause()
+smile(50, 50, sd.COLOR_DARK_ORANGE)
+
+
+
+
+sd.pause()
