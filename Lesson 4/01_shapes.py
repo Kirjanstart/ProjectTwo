e49dd47244aa48fafc0fa7c99c3337c830017f7d
k.lavkin <kirjanstart@bk.ru>
2023-06-15 16:51:16 +0300

    Function is done, break doesn't fix

--- Lesson 4/01_shapes.py
+++ Lesson 4/01_shapes.py
@@ -78,42 +78,65 @@ def hexagon(point, angle, length):
     v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
     v1.draw()
 
-    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 60, length=length, width=3)
+    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 30, length=length, width=3)
     v2.draw()
 
-    v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 120, length=length, width=3)
+    v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 60, length=length, width=3)
     v3.draw()
 
-    v4 = sd.get_vector(start_point=v3.end_point, angle=angle + 180, length=length, width=3)
+    v4 = sd.get_vector(start_point=v3.end_point, angle=angle + 90, length=length, width=3)
     v4.draw()
 
-    v5 = sd.get_vector(start_point=v4.end_point, angle=angle + 240, length=length, width=3)
+    v5 = sd.get_vector(start_point=v4.end_point, angle=angle + 120, length=length, width=3)
     v5.draw()
 
-    v6 = sd.get_vector(start_point=v5.end_point, angle=angle + 300, length=length, width=3)
+    v6 = sd.get_vector(start_point=v5.end_point, angle=angle + 150, length=length, width=3)
     v6.draw()
 
+    v7 = sd.get_vector(start_point=v6.end_point, angle=angle + 180, length=length, width=3)
+    v7.draw()
+
+    v8 = sd.get_vector(start_point=v7.end_point, angle=angle + 210, length=length, width=3)
+    v8.draw()
+
+    v9 = sd.get_vector(start_point=v8.end_point, angle=angle + 240, length=length, width=3)
+    v9.draw()
+
+    v10 = sd.get_vector(start_point=v9.end_point, angle=angle + 270, length=length, width=3)
+    v10.draw()
+
+    v11 = sd.get_vector(start_point=v10.end_point, angle=angle + 300, length=length, width=3)
+    v11.draw()
+
+    v12 = sd.get_vector(start_point=v11.end_point, angle=angle + 330, length=length, width=3)
+    v12.draw()
+
+def nangle(n, point, angle, length):
+    delta = (360 / n)
+    # sumangle = (n-2) * 180
+    # delta = sumangle//n
+    while n > 0:
+        v = sd.get_vector(start_point=point, angle=angle - n * delta, length=length, width=3)
+        v.draw()
+        n -= 1
+        point = v.end_point
 
 
 
 
+point_1 = sd.get_point(300, 100)
 
 
-
-point_0 = sd.get_point(300, 300)
-point_1 = sd.get_point(300, 0)
-
-
-triangle(point=point_1, angle=0, length=300)
-square(point=point_1, angle=0, length=300)
-pentagram(point=point_1, angle=0, length=300)
-hexagon(point=point_1, angle=0, length=300)
-
+# triangle(point=point_1, angle=0, length=300)
+# square(point=point_1, angle=0, length=300)
+# pentagram(point=point_1, angle=0, length=300)
+# hexagon(point=point_1, angle=0, length=100)
+nangle(n = 12, point=point_1, angle=0, length=100)
 
 
 
 # Часть 1-бис.
-# Попробуйте прикинуть обьем работы, если нужно будет внести изменения в этот код.
+# Попробуйте прикинуть объем работы, если нужно будет внести изменения в этот код.
 # Скажем, связывать точки не линиями, а дугами. Или двойными линиями. Или рисовать круги в угловых точках. Или...
 # А если таких функций не 4, а 44?
 
@@ -131,7 +154,7 @@ hexagon(point=point_1, angle=0, length=300)
 #   в начальной/конечной точках рисуемой фигуры (если он есть)
 
 # Часть 2-бис.
-# А теперь - сколько надо работы что бы внести изменения в код? Выгода на лицо :)
+# А теперь - сколько надо работы, чтобы внести изменения в код? Выгода на лицо. :)
 # Поэтому среди программистов есть принцип D.R.Y. https://clck.ru/GEsA9
 # Будьте ленивыми, не используйте копи-пасту!
 
