a54265cbd39b40d2299f2ba819bd58f3cabde972
k.lavkin <kirjanstart@bk.ru>
2023-06-20 17:28:47 +0300

    TEST file UPDATE

--- Lesson 2/TEST.py
+++ Lesson 2/TEST.py
@@ -1,18 +1,42 @@
-def elephant_to_free(some_list):
-    elephant_found = 'elephant' in some_list
-    if elephant_found:
-        some_list.remove('elephant')
-        print('Слон на свободе!!!')
-    return elephant_found
+# -*- coding: utf-8 -*-
+
+import simple_draw as sd
+
+sd.resolution = (1200, 600)
+
+# нарисовать ветку дерева из точки (300, 5) вертикально вверх длиной 100
 
 
-zoo = ['lion', 'elephant', 'monkey', 'skunk', 'horse', 'elephant']
 
-elephant_to_free(tuple(zoo))
-print(zoo)
+# sd.snowflake(center=point_0, length=200, factor_a=0.5, factor_b=0.1)
+y = 500
+x = 100
 
-elephant_to_free(zoo)
-print(zoo)
+x2 = 150
+y2 = 450
+while True:
+    sd.clear_screen()
+    point = sd.get_point(x, y)
+    sd.snowflake(center=point, length=50)
+    y -= 10
+    if y<50:
+        break
+    x = x * 1.05
 
-elephant_to_free(zoo)
-print(zoo)
+
+    point2 = sd.get_point(x2, y2)
+    sd.snowflake(center=point2, length=50)
+    y2 -= 10
+    if y<50:
+        break
+    x2 = x2 * 1.05
+    sd.sleep(0.1)
+    if sd.user_want_exit():
+        break
+
+
+
+
+
+
+sd.pause()
