2adb8d152b7db60cb8bd3f043e09b8eea8ebbb5f
k.lavkin <kirjanstart@bk.ru>
2023-06-21 17:40:48 +0300

    Snowflakes in procces

--- Lesson 4/05_snowfall.py
+++ Lesson 4/05_snowfall.py
@@ -2,13 +2,23 @@
 
 import simple_draw as sd
 
+sd.resolution = (1200, 600)
 
 # На основе кода из практической части реализовать снегопад:
 # - создать списки данных для отрисовки N снежинок
 # - нарисовать падение этих N снежинок
 # - создать список рандомных длин лучей снежинок (от 10 до 100) и пусть все снежинки будут разные
 
+snowflakes = []
 N = 20
+while N > 0:
+    i = sd.random_number(10, 100)
+    snowflakes.append(i)
+    N -= 1
+
+print(snowflakes)
+
+
 
 # Пригодятся функции
 # sd.get_point()
@@ -18,14 +28,59 @@ N = 20
 # sd.user_want_exit()
 
 # TODO здесь ваш код
-while True:
-    sd.clear_screen()
-    pass
-    pass
-    pass
-    sd.sleep(0.1)
-    if sd.user_want_exit():
-        break
+
+# point_0 = sd.get_point(sd.random_number(0,1000), sd.random_number(500, 600))
+
+# sd.snowflake(center=point_0, length=200, factor_a=0.5)
+
+
+y = sd.random_number(500, 600)
+x = sd.random_number(0,1000)
+
+y2 = sd.random_number(500, 600)
+x2 = sd.random_number(0,1000)
+sd.start_drawing()
+for length in snowflakes:
+
+    y = sd.random_number(500, 600)
+    x = sd.random_number(0, 1000)
+    while y > 10:
+        # sd.clear_screen()
+        point = sd.get_point(x, y)
+        sd.snowflake(center=point, length=length)
+        sd.snowflake(center=point, length=length, color=sd.background_color)
+        y -= 1
+        if y < 5:
+            break
+        x += 1
+        # sd.sleep()
+        sd.finish_drawing()
+        if sd.user_want_exit():
+            break
+
+
+
+
+# while True:
+#     sd.clear_screen()
+#     point = sd.get_point(x, y)
+#     sd.snowflake(center=point, length=50)
+#     y -= 10
+#     if y < 5:
+#        break
+#     x = x + 10
+#
+#     point2 = sd.get_point(x2, y2)
+#     sd.snowflake(center=point2, length=30)
+#     y2 -= 10
+#     if y2 < 5:
+#        break
+#     x2 = x2 + 20
+#
+#     sd.sleep(0.1)
+#     if sd.user_want_exit():
+#         break
+
 
 sd.pause()
 
