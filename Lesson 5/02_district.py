17116af299cd5d60a55c03b59db6bb9432918422
k.lavkin <kirjanstart@bk.ru>
2023-07-03 15:17:10 +0300

    Folks in the district were counted

--- Lesson 5/02_district.py
+++ Lesson 5/02_district.py
@@ -6,7 +6,41 @@
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
 # TODO здесь ваш код
+from pprint import pprint
+from district.central_street.house1.room1 import folks as cs_h1_r1
+from district.central_street.house1.room2 import folks as cs_h1_r2
+from district.central_street.house2.room1 import folks as cs_h2_r1
+from district.central_street.house2.room2 import folks as cs_h2_r2
 
+from district.soviet_street.house1.room1 import folks as ss_h1_r1
+from district.soviet_street.house1.room2 import folks as ss_h1_r2
+from district.soviet_street.house2.room1 import folks as ss_h2_r1
+from district.soviet_street.house2.room2 import folks as ss_h2_r2
 
+print('На районе живут: ', end='')
+for folk in cs_h1_r1:
+    folk = str(folk)
+    print(folk, end = ', ')
+for folk in cs_h1_r2:
+    folk = str(folk)
+    print(folk, end = ', ')
+for folk in cs_h2_r1:
+    folk = str(folk)
+    print(folk, end = ', ')
+for folk in cs_h2_r2:
+    folk = str(folk)
+    print(folk, end = ', ')
+for folk in ss_h1_r1:
+    folk = str(folk)
+    print(folk, end = ', ')
+for folk in ss_h1_r2:
+    folk = str(folk)
+    print(folk, end = ', ')
+for folk in ss_h2_r1:
+    folk = str(folk)
+    print(folk, end = ', ')
+for folk in ss_h2_r2:
+    folk = str(folk)
+    print(folk, end = ', ')
 
 
