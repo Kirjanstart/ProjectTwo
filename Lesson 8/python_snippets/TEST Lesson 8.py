b692041fab221d463a10f38909b79a44a8ddf7f0
k.lavkin <kirjanstart@bk.ru>
2023-09-19 16:52:59 +0300

    Practice step 1. Base form

---
+++ Lesson 8/python_snippets/TEST Lesson 8.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+
+from random import randint
+from termcolor import cprint
+
+# Реализуем модель доставки грузов
+
+# Дорога - хранит расстояния между обьектами
+# Склад - хранит груз и управляет очередями грузовиков
+
+# Базовый класс - Машина,
+# имеет
+#   кол-во топлива
+# может
+#   заправляться
+
+# Грузовик (производный от Машина)
+# имеет
+#   емкость кузова, скорость движения, расход топлива за час поездки
+# может
+#   стоят под погрузкой/разгрузкой
+#   ехать со скоростью
+
+# Погрузчик (производный от Машина)
+# имеет
+#   скорость погрузки, расход топлива в час при работе
+# может
+#   загружать/разгружать грузовик
+#   ждать грузовик
+class Road:
+
+    def __init__(self, start, end, distance):
+        self.start = start
+        self.end = end
+        self.distance = distance
+
+
+class Warehouse:
+
+    def __init__(self, name, content = 0):
+        pass
+
+    def __str__(self):
+        pass
+
+    def set_road_out(self, road):
+        pass
+
+    def truck_arrived(self, truck):
+        pass
+
+    def get_next_truck(self):
+        pass
+
+    def truck_ready(self, truck):
+        pass
+
+    def act(self):
+        pass
+
+
+class Veicle:
+    fuel_rate = 0
+
+    def __init__(self, model):
+        pass
+
+    def __str__(self):
+        pass
+
+    def tunk_up(self):
+        pass
+
+
+
+class Truck(Veicle):
+
+    def __init__(self, model, body_space=1000):
+        pass
+
+    def __str__(self):
+        pass
+
+    def ride(self):
+        pass
+
+    def go_to(self, road):
+        pass
+
+    def act(self):
+        pass
+
+
+class AutoLoader(Veicle):
+
+    def __init__(self, model, bucket_capacity=100, warehouse=None, role='loader'):
+        pass
+
+    def __str__(self):
+        pass
+
+    def act(self):
+        pass
+
+    def load(self):
+        pass
+
+    def unload(self):
+        pass
+
+
+TOTAL_CARGO = 100000
+
+moscow = Warehouse(name='Москва', content=TOTAL_CARGO)
+piter = Warehouse(name='Питер', content=0)
+
+moscow_piter = Road(start=moscow, end=piter, distance=715)
+piter_moscow = Road(start=piter, end=moscow, distance=780)
+
+moscow.set_road_out(moscow_piter)
+piter.set_road_out(piter_moscow)
+
+loader_1 = AutoLoader(model='Bobcat', bucket_capacity=1000, warehouse=moscow, role='loader')
+loader_2 = AutoLoader(model='Lonking', bucket_capacity=500, warehouse=piter, role='unloader')
+
+truck_1 = Truck(model='КАМАЗ', body_space=5000)
+truck_2 = Truck(model='ГАЗ', body_space=2000)
+
+moscow.truck_arrived(truck_1)
+moscow.truck_arrived(truck_2)
+
+hour = 0
+
+while piter.content < TOTAL_CARGO:
+    hour += 1
+
